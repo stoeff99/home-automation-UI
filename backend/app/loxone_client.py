7562c43d0e62bbbdf3f6f1fe36aecf96c7837001
stoeff99 <35835293+stoeff99@users.noreply.github.com>
2025-11-19 20:54:12 +0100

    Add placeholder Loxone client class

---
+++ backend/app/loxone_client.py
@@ -0,0 +1,14 @@
+class LoxoneClient:
+    """
+    Placeholder Loxone client.
+    Extend with real API calls when ready.
+    """
+
+    def __init__(self, host, username, password):
+        self.host = host
+        self.username = username
+        self.password = password
+
+    def get_sensor_values(self):
+        # TODO: Implement real Loxone communication
+        return []
